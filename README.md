d3708201d5f47d948b8357bb3e199d02bc7bc235
mattcrouse <102341630+mattcrouse@users.noreply.github.com>
2022-09-29 15:49:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-week5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dhajgq)
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Peter Lee - Project Owner/Scrum Master for Sprint 1' },
+  { id: 2, name: 'James Warren - Database/Builder' },
+  { id: 3, name: 'Matt Crouse - Frontend/Client-side Programmer' },
+  { id: 4, name: 'Nancy Do - Cloud/Designer' },
+  { id: 5, name: 'Amber Czerwinski - Security/Designer' },
+  { id: 6, name: 'Tony Tran - Backend/Server-side Programmer' },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
